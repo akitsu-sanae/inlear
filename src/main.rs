e85bfeb19e41ab95f2ebbacb6674973530270dff
akitsu-sanae <akitsu.sanae@gmail.com>
2017-06-24 12:31:05 +0900

    add pair type check

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,14 @@ pub enum Qual {
     Unlinear,
 }
 
+impl Qual {
+    fn check(self, ty: &Type) -> bool {
+        match ty {
+            &(p, _) => !(self == Qual::Unlinear && p == Qual::Linear),
+        }
+    }
+}
+
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Term {
     Var(String),
@@ -40,6 +48,7 @@ enum TypeError {
     VariableType(String, Type, Type),
     Unmatch(Type, Type),
 
+    Others(String),
     Unimplemented
 }
 
@@ -74,7 +83,23 @@ fn type_check(t: &Term, ty: &Type, context: &Context) -> Result<Context, TypeErr
             let context = type_check(then, ty, &context)?;
             let context = type_check(else_, ty, &context)?;
             Ok(context)
-        }
+        },
+        Pair(ref q, box ref t1, box ref t2) => {
+            if let &(ref q_, PreType::Pair(box ref ty1, box ref ty2)) = ty {
+                if q != q_ {
+                    return Err(TypeError::Others(format!("not match: {:?} and {:?}", q, q_)))
+                }
+                let context = type_check(t1, ty1, context)?;
+                let context = type_check(t2, ty2, &context)?;
+                if q.check(ty1) && q.check(ty2) {
+                    Ok(context)
+                } else {
+                    Err(TypeError::Others(format!("not satisfy qualitifier condition: {:?}({:?}, {:?})", q, ty1, ty2)))
+                }
+            } else {
+                Err(TypeError::Others(format!("not pair: {:?}", ty)))
+            }
+        },
         _ => Err(TypeError::Unimplemented)
     }
 }
